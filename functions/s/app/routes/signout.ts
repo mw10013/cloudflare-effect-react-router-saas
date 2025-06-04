c6e6a7b436fd2c76706fa5a825fcc01057022681
Michael Wu <mw10013@gmail.com>
2025-06-04 16:02:22 -0400

    Refactor signout action: improve context handling and clean up code formatting

--- functions/s/app/routes/signout.ts
+++ functions/s/app/routes/signout.ts
@@ -1,15 +1,15 @@
-import type { Route } from './+types/signout'
-import { Effect } from 'effect'
-import { redirect } from 'react-router'
-import * as ReactRouter from '~/lib/ReactRouter'
+import type { Route } from "./+types/signout";
+import { Effect } from "effect";
+import { redirect } from "react-router";
+import * as ReactRouter from "~/lib/ReactRouter";
 
 export const action = ReactRouter.routeEffect(({ context }: Route.ActionArgs) =>
   Effect.gen(function* () {
-    const appLoadContext = context.get(ReactRouter.appLoadContext)
+    const appLoadContext = yield* ReactRouter.AppLoadContext;
     context.set(ReactRouter.appLoadContext, {
       ...appLoadContext,
-      sessionAction: 'destroy'
-    })
-    return redirect('/')
-  })
-)
+      sessionAction: "destroy",
+    });
+    return redirect("/");
+  }),
+);
