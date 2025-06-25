d6040899c352db2992879da7129a3b21561ee633
Michael Wu <mw10013@gmail.com>
2025-06-25 11:49:06 -0400

    refactor: streamline AI route by integrating OpenAI client and updating environment variable usage for improved API interaction

--- functions/s/app/routes/app.$accountId.ai1.api.ts
+++ functions/s/app/routes/app.$accountId.ai1.api.ts
@@ -2,47 +2,28 @@ import type { Route } from "./+types/app.$accountId.ai1.api";
 import { createOpenAI } from "@ai-sdk/openai";
 import { streamText } from "ai";
 import { Effect } from "effect";
-import { createWorkersAI } from "workers-ai-provider";
 import * as ReactRouterEx from "~/lib/ReactRouterEx";
 
 export const action = ReactRouterEx.routeEffect(
   ({ request }: Route.ActionArgs) =>
     Effect.gen(function* () {
+      const env = (yield* ReactRouterEx.AppLoadContext).cloudflare.env;
       const { messages } = yield* Effect.tryPromise(
         () => request.json() as Promise<{ messages: any[] }>,
       );
-      const workersai = createWorkersAI({
-        binding: (yield* ReactRouterEx.AppLoadContext).cloudflare.env.AI,
+      const openai = createOpenAI({
+        apiKey: env.CF_WORKERS_AI_API_TOKEN,
+        // OpenAI client automatically adds /chat/completions to the end of the baseURL
+        baseURL: `https://gateway.ai.cloudflare.com/v1/${env.CF_ACCOUNT_ID}/${env.CF_AI_GATEWAY_ID}/compat`,
+        headers: {
+          "cf-aig-authorization": `Bearer ${env.CF_AI_GATEWAY_TOKEN}`,
+        },
+        compatibility: "strict", // strict mode, enable when using the OpenAI API
       });
       const result = streamText({
-        model: workersai("@cf/meta/llama-3.1-8b-instruct"),
+        model: openai("workers-ai/@cf/meta/llama-3.1-8b-instruct"),
         messages,
       });
       return result.toDataStreamResponse();
     }),
 );
-
-// export const action = ReactRouterEx.routeEffect(({ request }: Route.ActionArgs) =>
-//   Effect.gen(function* () {
-//     const env = (yield* ReactRouterEx.AppLoadContext).cloudflare.env;
-//     const { messages } = yield* Effect.tryPromise(
-//       () => request.json() as Promise<{ messages: any[] }>,
-//     );
-//     const openai = createOpenAI({
-//       name: "workers-ai",
-//       // modelId: "workers-ai/@cf/meta/llama-3.1-8b-instruct",
-//       baseURL: ` https://gateway.ai.cloudflare.com/${env.CF_ACCOUNT_ID}/saas-ai-gateway/workers-ai/v1/chat/completions`,
-//       apiKey: env.CF_WORKERS_AI_API_TOKEN,
-//       compatibility: "strict", // strict mode, enable when using the OpenAI API
-//       headers: {
-//         "cf-aig-authorization": `Bearer ${env.CF_AI_GATEWAY_TOKEN}`,
-//         // Authorization: `Bearer ${env.CF_AI_GATEWAY_TOKEN}`,
-//       },
-//     });
-//     const result = streamText({
-//       model: openai("workers-ai/@cf/meta/llama-3.1-8b-instruct"),
-//       messages,
-//     });
-//     return result.toDataStreamResponse();
-//   }),
-// );
