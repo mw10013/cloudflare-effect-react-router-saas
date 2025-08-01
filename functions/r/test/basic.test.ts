8fb9756acfc9e740be46a779ecde6018c95bc0c1
Michael Wu <mw10013@gmail.com>
2025-08-01 13:56:40 -0400

    chore: update wrangler to version 4.27.0 and adjust related dependencies
    - Added a new script "typecheck:test" in package.json for testing TypeScript configurations.
    - Reorganized imports in basic.test.ts for clarity.
    - Updated worker-configuration.d.ts to reflect the new workerd version and added MessageEvent and MessagePort interfaces.
    - Modified tsconfig.node.json to comment out the "types" field for node.
    - Updated pnpm-lock.yaml to reflect the new wrangler version and its dependencies.
    - Updated pnpm-workspace.yaml to reference the new wrangler version.

--- functions/r/test/basic.test.ts
+++ functions/r/test/basic.test.ts
@@ -3,8 +3,8 @@ import {
   env,
   waitOnExecutionContext,
 } from "cloudflare:test";
-import worker from "./test-worker";
 import { describe, expect, it } from "vitest";
+import worker from "./test-worker";
 
 describe("basic test", () => {
   it("should pass", () => {
@@ -17,19 +17,17 @@ describe("basic test", () => {
   });
 
   // This will improve in the next major version of `@cloudflare/workers-types`,
-// but for now you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
-
+  // but for now you'll need to do something like this to get a correctly-typed
+  // `Request` to pass to `worker.fetch()`.
+  const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
   it("dispatches fetch event", async () => {
-	const request = new IncomingRequest("http://example.com");
-	const ctx = createExecutionContext();
-	const response = await worker.fetch(request, env, ctx);
-	await waitOnExecutionContext(ctx);
-  console.log({ response });
-  expect(response.status).toBe(200);
-	// expect(await response.text()).toBe("👋 http://example.com");
-});
-
+    const request = new IncomingRequest("http://example.com");
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    await waitOnExecutionContext(ctx);
+    console.log({ response });
+    expect(response.status).toBe(200);
+    // expect(await response.text()).toBe("👋 http://example.com");
+  });
 });
