eab0f878c3a111acd8ef6bbee065b9fdaeec8ed3
Michael Wu <mw10013@gmail.com>
2025-08-01 18:08:05 -0400

    refactor: update test cases and tsconfig for improved clarity and functionality

--- functions/r/test/basic.test.ts
+++ functions/r/test/basic.test.ts
@@ -4,15 +4,11 @@ import {
   waitOnExecutionContext,
 } from "cloudflare:test";
 import { describe, expect, it } from "vitest";
+import { loader } from "~/routes/home";
 import worker from "./test-worker";
 
-describe("basic test", () => {
-  it("should pass", () => {
-    expect(1 + 1).toBe(2);
-  });
-
+describe("basic", () => {
   it("should have env", () => {
-    console.log({ env });
     expect(env).toBeDefined();
   });
 
@@ -24,5 +20,13 @@ describe("basic test", () => {
     const response = await worker.fetch(request, env, ctx);
     await waitOnExecutionContext(ctx);
     expect(response.status).toBe(200);
+  });
+
+  it("home loader returns expected message", () => {
+    const context = {
+      cloudflare: { env },
+    };
+    const result = loader({ context });
+    expect(result).toEqual({ message: env.VALUE_FROM_CLOUDFLARE });
   });
 });
