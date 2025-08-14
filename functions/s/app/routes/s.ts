8ee24d44a22a6682ccf1eb28c9a1d24e35e4bed4
Michael Wu <mw10013@gmail.com>
2025-08-14 18:39:43 -0400

    feat: enhance loader function with session handling and user creation logic

--- functions/s/app/routes/s.ts
+++ functions/s/app/routes/s.ts
@@ -1,21 +1,69 @@
+import type { User } from "better-auth/types";
 import type { Route } from "./+types/s";
-import { redirect } from "react-router";
+import { redirect, unstable_RouterContextProvider } from "react-router";
 import { createAuth } from "~/lib/auth";
 import { appLoadContext } from "~/lib/middleware";
 
-export async function loader({ request, context }: Route.ActionArgs) {
-  const { cloudflare } = context.get(appLoadContext);
+export async function loader({ context }: Route.ActionArgs) {
+  const alc = context.get(appLoadContext);
   let magicLinkToken: string;
-  const auth = createAuth({
-    d1: cloudflare.env.D1,
+  const auth = await createAuth({
+    d1: alc.cloudflare.env.D1,
     sendMagicLink: async ({ token }) => {
       magicLinkToken = token;
     },
   });
-  // const response = await auth.api.signOut({
-  //   headers: request.headers,
-  //   asResponse: true,
-  // });
-  // if (!response.ok) throw response;
+
+  const ctx = async ({ headers }: { headers?: Headers } = {}) => {
+    const session = headers
+      ? ((await auth.api.getSession({ headers })) ?? undefined)
+      : undefined;
+    const context = new unstable_RouterContextProvider();
+    context.set(appLoadContext, { ...alc, auth, session });
+    return context;
+  };
+
+  const sessionCookie = (response: Response) => {
+    const setCookieHeader = response.headers.get("Set-Cookie")!;
+    const match = setCookieHeader.match(/better-auth\.session_token=([^;]+)/);
+    if (!match) throw new Error(`Missing session cookie: ${setCookieHeader}`);
+    return `better-auth.session_token=${match[1]}`;
+  };
+
+  const createUser = async (email: User["email"]) => {
+    const user = {
+      email,
+      headers: new Headers(),
+      context: async () => ctx({ headers: user.headers }),
+      session: async () => await auth.api.getSession({ headers: user.headers }),
+    };
+    const signInMagicLinkResponse = await auth.api.signInMagicLink({
+      asResponse: true,
+      headers: {},
+      body: { email: user.email },
+    });
+    if (signInMagicLinkResponse.status !== 200)
+      throw new Error("createUser: failed to signInMagicLink", {
+        cause: signInMagicLinkResponse,
+      });
+    const magicLinkVerifyResponse = await auth.api.magicLinkVerify({
+      asResponse: true,
+      headers: {},
+      query: {
+        token: magicLinkToken,
+        // No callbackURL's so response is not redirect and we can check for status 200
+      },
+    });
+    if (magicLinkVerifyResponse.status !== 200)
+      throw new Error("createUser: failed to verify magic link", {
+        cause: magicLinkVerifyResponse,
+      });
+    user.headers.set("Cookie", sessionCookie(magicLinkVerifyResponse));
+    return user;
+  };
+
+  const u = await createUser("u@u.com");
+  const u1 = await createUser("u1@u.com");
+  const u2 = await createUser("u2@u.com");
   return redirect("/");
 }
