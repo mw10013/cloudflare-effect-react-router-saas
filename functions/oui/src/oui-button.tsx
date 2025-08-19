953812cf56bce55818d5eb13e37e6b50ae8c3374
Michael Wu <mw10013@gmail.com>
2025-08-19 09:54:02 -0400

    fix: remove redundant outline-none class from buttonVariants

--- functions/oui/src/oui-button.tsx
+++ functions/oui/src/oui-button.tsx
@@ -12,7 +12,7 @@ export const buttonVariants = cva(
   [
     focusVisibleStyles,
     disabledStyles,
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0",
   ],
   {
     variants: {
