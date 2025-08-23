c93f48fa21a26adba5e98161e671b0217cd3effe
Michael Wu <mw10013@gmail.com>
2025-08-23 12:37:50 -0400

    refactor: enhance user management table with detailed user information and improved layout

--- functions/s/app/routes/admin.users.tsx
+++ functions/s/app/routes/admin.users.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/admin.users";
 import { invariant } from "@epic-web/invariant";
+import * as Oui from "@workspace/oui";
 import { appLoadContext } from "~/lib/middleware";
 
 export async function loader({ request, context }: Route.LoaderArgs) {
@@ -17,14 +18,77 @@ export async function loader({ request, context }: Route.LoaderArgs) {
   return result;
 }
 
+/*
+loaderData:
+
+"users": [
+    {
+      "name": "Admin",
+      "email": "a@a.com",
+      "emailVerified": true,
+      "image": null,
+      "createdAt": "2025-08-22T19:07:35.000Z",
+      "updatedAt": "2025-08-22T19:07:35.000Z",
+      "role": "admin",
+      "banned": false,
+      "banReason": null,
+      "banExpires": null,
+      "id": "1"
+    },
+    {
+      "name": "",
+      "email": "u1@u.com",
+      "emailVerified": true,
+      "image": null,
+      "createdAt": "2025-08-22T15:16:21.542Z",
+      "updatedAt": "2025-08-22T15:16:21.542Z",
+      "role": "user",
+      "banned": false,
+      "banReason": null,
+      "banExpires": null,
+      "id": "3"
+    },
+  ],
+  "total": 2,
+  "limit": 5
+*/
+
 export default function RouteComponent({ loaderData }: Route.ComponentProps) {
   return (
     <div className="flex flex-col gap-8 p-6">
       <header>
         <h1 className="text-3xl font-bold tracking-tight">Users</h1>
-        <p className="text-muted-foreground text-sm">Manage users.</p>
+        <p className="text-muted-foreground text-sm">
+          Manage your users and roles.
+        </p>
       </header>
-      <pre>{JSON.stringify(loaderData, null, 2)}</pre>
+
+      <Oui.Table aria-label="Users">
+        <Oui.TableHeader>
+          <Oui.Column isRowHeader className="w-8">
+            Id
+          </Oui.Column>
+          <Oui.Column>Email</Oui.Column>
+          <Oui.Column>Role</Oui.Column>
+          <Oui.Column>Verified</Oui.Column>
+          <Oui.Column>Banned</Oui.Column>
+          <Oui.Column>Ban Reason</Oui.Column>
+          <Oui.Column>Created</Oui.Column>
+        </Oui.TableHeader>
+        <Oui.TableBody items={loaderData.users}>
+          {(user) => (
+            <Oui.Row id={user.id}>
+              <Oui.Cell>{user.id}</Oui.Cell>
+              <Oui.Cell>{user.email}</Oui.Cell>
+              <Oui.Cell>{user.role}</Oui.Cell>
+              <Oui.Cell>{String(user.emailVerified)}</Oui.Cell>
+              <Oui.Cell>{String(user.banned)}</Oui.Cell>
+              <Oui.Cell>{user.banReason ?? ""}</Oui.Cell>
+              <Oui.Cell>{user.createdAt.toLocaleString()}</Oui.Cell>
+            </Oui.Row>
+          )}
+        </Oui.TableBody>
+      </Oui.Table>
     </div>
   );
 }
