9f9e9daea69a223320a0f60892e87f7827f14281
Michael Wu <mw10013@gmail.com>
2025-08-24 10:33:07 -0400

    refactor: update error message in ErrorBoundary for clarity

--- functions/s/app/root.tsx
+++ functions/s/app/root.tsx
@@ -75,7 +75,7 @@ export default function App() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
+  let message = "A dreadful error occurred.";
   let details = "An unexpected error occurred.";
   let stack: string | undefined;
 
