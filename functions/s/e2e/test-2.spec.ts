cab85943b264c4d5803daf3253ca9777b84a0e2c
Michael Wu <mw10013@gmail.com>
2025-09-04 18:19:38 -0400

    test: streamline user deletion test by removing redundant checks and improving flow

--- functions/s/e2e/test-2.spec.ts
+++ functions/s/e2e/test-2.spec.ts
@@ -17,22 +17,17 @@ test("test", async ({ page }) => {
     page.getByRole("link", { name: "http://localhost:5173/api/" }),
   ).toBeVisible();
   await page.getByRole("link", { name: "http://localhost:5173/api/" }).click();
-  await expect(
-    page.getByRole("button", { name: "Toggle sidebar" }),
-  ).toBeVisible();
+  await page.waitForURL("**/admin**");
+  await expect(page.getByText("E2E")).toBeVisible();
   await page.getByText("E2E").click();
   await expect(
     page.locator("div").filter({ hasText: /^Delete User$/ }),
   ).toBeVisible();
+  await expect(page.getByRole("textbox", { name: "Email" })).toBeVisible();
   await page.getByRole("textbox", { name: "Email" }).click();
   await page.getByRole("textbox", { name: "Email" }).fill("e2e@e2e.com");
+  await expect(page.getByRole("button", { name: "Delete User" })).toBeVisible();
   await page.getByRole("button", { name: "Delete User" }).click();
+  await expect(page.getByRole("button", { name: "Delete User" })).toBeVisible();
   await page.getByRole("button", { name: "Delete User" }).click();
-
-  // await expect(page.getByText('E2E')).toBeVisible();
-  // await page.getByText('E2E').click();
-  // await expect(page.getByRole('textbox', { name: 'Email' })).toBeVisible();
-  // await page.getByRole('textbox', { name: 'Email' }).click();
-  // await page.getByRole('textbox', { name: 'Email' }).fill('e2e@e2e.com');
-  // await page.getByRole('button', { name: 'Delete User' }).click();
 });
