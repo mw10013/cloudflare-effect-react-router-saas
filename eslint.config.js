73ea66fd7e7867cc0bdfdaf243dd1a59340ab5da
Michael Wu <mw10013@gmail.com>
2025-09-14 20:43:16 -0400

    refactor: remove TypeScript check and update ESLint configuration for better type checking

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
@@ -13,8 +11,23 @@ export default defineConfig(
   ]),
 
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  // tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylistic,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: [
+            "*.js",
+            "*.ts",
+            "*.mjs",
+            "functions/s/scripts/*.ts",
+          ],
+        },
+      },
+    },
+  },
   {
     rules: {
       "no-unused-vars": "off",
