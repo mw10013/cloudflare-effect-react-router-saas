4b82f934714aaed4bba89167486a92a06087bd5b
Michael Wu <mw10013@gmail.com>
2025-09-16 12:42:26 -0400

    refactor: simplify fetch function by using arrow function and Promise.resolve

--- functions/s/test/test-worker.ts
+++ functions/s/test/test-worker.ts
@@ -1,5 +1,4 @@
 export default {
-  fetch(request, _env, _ctx) {
-    return new Response(`👋 ${request.url}`);
-  },
+  fetch: (request, _env, _ctx) =>
+    Promise.resolve(new Response(`👋 ${request.url}`)),
 } satisfies ExportedHandler<Env>;
