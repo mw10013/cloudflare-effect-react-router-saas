62f7963f9147cc2c878f8e89a8e766061cddcbbf
Michael Wu <mw10013@gmail.com>
2025-09-21 08:17:46 -0400

    chore: restore cloudflare:test module declaration with ProvidedEnv and runInDurableObject function

--- functions/s/test/cloudflare-test.d.ts
+++ functions/s/test/cloudflare-test.d.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 declare module "cloudflare:test" {
+  // ProvidedEnv controls the type of `import("cloudflare:test").env`
+  interface ProvidedEnv extends Env {
+    TEST_MIGRATIONS: D1Migration[];
+  }
+
   /**
    * Run code within a Durable Object instance.
    *
--- functions/s/test/env.d.ts
+++
@@ -1,7 +0,0 @@
-
-declare module "cloudflare:test" {
-  // ProvidedEnv controls the type of `import("cloudflare:test").env`
-  interface ProvidedEnv extends Env {
-    TEST_MIGRATIONS: D1Migration[];
-  }
-}
