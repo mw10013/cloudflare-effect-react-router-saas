9dab4f4f04af451e1d1f72849016cf6d5d5a8e9b
Michael Wu <mw10013@gmail.com>
2025-10-01 16:53:18 -0400

    refactor: update Tailwind CSS configuration and enhance Header component structure

--- functions/x/app/routes/_index.tsx
+++ functions/x/app/routes/_index.tsx
@@ -1,3 +1,15 @@
 export default function RouteComponent() {
-  return <div className="flex min-h-svh flex-row justify-center gap-2 p-6">Index</div>
+  return (
+    <div data-home>
+      <div className="max-w-3xl max-sm:text-center">
+        <h1 className="font-heading mb-4 text-4xl/[1.1] font-bold tracking-tight text-foreground md:text-5xl/[1.1]">
+          React Aria Components with Shadcn characteristics.
+        </h1>
+        <p className="mb-8 text-lg text-muted-foreground">
+          An open-source collection of copy-and-paste components for UIs.
+        </p>
+        {/* <SearchButton /> */}
+      </div>
+    </div>
+  );
 }
--- functions/x/components/header.tsx
+++ functions/x/components/header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="relative mb-14">
       <div className="mx-auto flex h-[72px] w-full max-w-6xl items-center justify-between gap-3">
-        <Link href="/">Oui</Link>
+        <Link href="/" className="shrink-0">
+          Oui
+        </Link>
       </div>
     </header>
   );
