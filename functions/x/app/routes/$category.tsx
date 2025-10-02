485729fb14cf8a050bd5661c8506500f7e07571f
Michael Wu <mw10013@gmail.com>
2025-10-02 10:19:19 -0400

    feat: implement PageGrid component and update category route to display components with button demos

--- functions/x/app/routes/$category.tsx
+++ functions/x/app/routes/$category.tsx
@@ -1,6 +1,8 @@
-import { getComponentsByNames } from "@/lib/utils";
 import type { Route } from "./+types/$category";
+import ComponentCard from "@/components/components-card";
+import PageGrid from "@/components/page-grid";
 import { getCategory } from "@/config/components";
+import { getComponentsByNames } from "@/lib/utils";
 import ButtonDemo from "@/registry/components/button-demo";
 import ButtonDisabledDemo from "@/registry/components/button-demo-disabled";
 import { invariant } from "@epic-web/invariant";
@@ -16,12 +18,25 @@ export function loader({ params }: Route.LoaderArgs) {
   return { category, components };
 }
 
-export default function RouteComponent({ loaderData }: Route.ComponentProps) {
+export default function RouteComponent({
+  loaderData: { components },
+}: Route.ComponentProps) {
   return (
     <div className="p-6">
-      <ButtonDemo />
-      <ButtonDisabledDemo />
-      <pre>{JSON.stringify(loaderData.category, null, 2)}</pre>
+      <PageGrid>
+        {components.map((component) => (
+          <ComponentCard key={component.name} component={component}>
+            {component.name}
+            {/* <ComponentLoader component={component} />
+            <ComponentDetails component={component} /> */}
+          </ComponentCard>
+        ))}
+      </PageGrid>
+      <div className="flex gap-2">
+        <ButtonDemo />
+        <ButtonDisabledDemo />
+      </div>
+      <pre>{JSON.stringify({ components }, null, 2)}</pre>
     </div>
   );
 }
