8b39c283360725a2f0d63cc5cf289eca6fea8e7b
Michael Wu <mw10013@gmail.com>
2025-10-06 09:06:45 -0400

    feat: add oui-menu-ex-demo component and update registry entries

--- functions/x/config/categories.ts
+++ functions/x/config/categories.ts
@@ -143,7 +143,7 @@ export const categories: Category[] = [
   {
     name: "Menu",
     slug: "menu",
-    components: [{ name: "oui-menu-ex" }],
+    components: [{ name: "oui-menu-ex-demo" }, { name: "oui-menu-ex" }],
   },
   {
     name: "Modal",
